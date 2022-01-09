83f76476adca6e71ee0211d3239d9d5d5ebf40a0
Keita Katahira <katahirakeita1201@gmail.com>
2022-01-09 09:26:39 +0900

    [Add] Active Storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  has_one_attached :image
+
   validates :content, presence: true, length: { maximum: 255 }
   validates :rate, numericality: { in: 1..5 }
 end
