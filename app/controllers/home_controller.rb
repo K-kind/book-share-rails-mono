80037f659ee317125badb3f014efc8b0ea52ca87
Keita Katahira <katahirakeita1201@gmail.com>
2022-01-09 20:56:52 +0900

    [Add] posts index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   before_action :require_login, only: [:index]
 
-  def index; end
+  def index
+    @posts = current_user.posts.order(created_at: :desc)
+  end
 end
